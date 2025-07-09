a92645bc6f9a20eb2c87b2036598ff02bce426af
Newinerstudio <newinderstudio@gmail.com>
2025-07-09 20:15:18 +0900

    Test Example

--- src/feature/example/ExampleScreen.tsx
+++ src/feature/example/ExampleScreen.tsx
@@ -1,10 +1,30 @@
 "use client"
 
-function ExampleScreen() {
+import { Flex, FlexRow } from "@/common/styledComponents";
+import { use } from "react";
+
+type SearchParams = Promise<{ test: string, code: string }>;
+
+function ExampleScreen({ searchParams }: { searchParams: SearchParams }) {
+
+  const { code, test } = use(searchParams);
+
   return (
-    <h1>
-      ExampleScreen
-    </h1>
+    <Flex>
+      <h1>
+        테스트 페이지입니다.
+      </h1>
+      <Flex>
+        <FlexRow>
+          <p>코드: </p>
+          <div>{code}</div>
+        </FlexRow>
+        <FlexRow>
+          <p>테스트: </p>
+          <div>{test}</div>
+        </FlexRow>
+      </Flex>
+    </Flex>
   );
 };
 
