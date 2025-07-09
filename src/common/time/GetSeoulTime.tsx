4e1ea3a1bdf0a6bb20067f8d2ebf37d5b5bd0227
Newinerstudio <newinderstudio@gmail.com>
2025-07-09 19:30:27 +0900

    Fix Dayjs

--- src/common/time/GetSeoulTime.tsx
+++ src/common/time/GetSeoulTime.tsx
@@ -1,7 +1,7 @@
 import "dayjs/locale/ko";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/TIMEZONE";
+import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
